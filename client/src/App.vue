f62be794d3b66115ac87adf82fc8590ccded2384
Paulo Henrique <phvcandido@gmail.com>
2022-10-29 10:17:49 -0300

    add tailwind start setup

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <header>Example Todo List (SpringBoot + VueJS)</header>
-  <RouterView />
+  <main class="px-72 p-8">
+    <header class="flex justify-between items-center">
+      <h1 class="text-4xl">Example Todo List</h1>
+      <router-link
+        class="bg-blue-500 px-4 p-2 text-white rounded hover:bg-blue-700 transition-colors"
+        to="/newTask"
+      >
+        New Task
+      </router-link>
+    </header>
+    <RouterView />
+  </main>
 </template>
 
 <style scoped></style>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,7 +1,54 @@
-<script setup lang="ts">
-import TheWelcome from "../components/TheWelcome.vue";
-</script>
-
 <template>
-  <main>Hi friend</main>
+  <main class="mt-8">
+    <h4>All todos:</h4>
+    <div id="containerTodos" class="mt-4 flex gap-4 flex-col">
+      <div
+        class="bg-slate-100 rounded p-4 justify-between flex items-center"
+        v-for="todo in todos"
+        :key="todo.id"
+      >
+        <h5
+          class="taxed"
+          :class="todo.todoStatus === 'COMPLETED' ? 'line-through' : ''"
+        >
+          {{ todo.title }}
+        </h5>
+        <button
+          class="text-sm p-2 text-white rounded transition-colors"
+          :class="
+            todo.todoStatus === 'COMPLETED'
+              ? 'bg-red-500 hover:bg-red-600'
+              : 'bg-blue-500 hover:bg-blue-600'
+          "
+        >
+          {{
+            todo.todoStatus === "COMPLETED"
+              ? "Mark as incomplete"
+              : "Mark as complete"
+          }}
+        </button>
+      </div>
+    </div>
+  </main>
 </template>
+<script lang="ts">
+import axios from "axios";
+
+export default {
+  name: "HomeView",
+  data: () => ({
+    message: "Hello from HomeView",
+    todos: [],
+  }),
+  mounted() {
+    console.log(this.message);
+    this.getTodos();
+  },
+  methods: {
+    async getTodos() {
+      const { data } = await axios.get("http://localhost:8080/api/v1/todo");
+      this.todos = data;
+    },
+  },
+};
+</script>
