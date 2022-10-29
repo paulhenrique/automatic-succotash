3a7eb605ebdd25870b122f09e8bf6c4f36a47a1b
Paulo Henrique <phvcandido@gmail.com>
2022-10-29 09:36:49 -0300

    Starting client setup

---
+++ client/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <header>Example Todo List (SpringBoot + VueJS)</header>
+  <RouterView />
+</template>
+
+<style scoped></style>
